4aedf139f5b343e7fcec779519384a342dc7c467
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-06 18:13:47 -0300

    feat: add image to empty cart

--- src/components/CartSidebar/components/EmptyCart/index.tsx
+++ src/components/CartSidebar/components/EmptyCart/index.tsx
@@ -4,7 +4,7 @@ const EmptyCart = () => (
   <div className="flex justify-center flex-col items-center h-5/6 overflow-hidden">
     <img
       className="w-full px-8"
-      src="https://www.99fashionbrands.com/wp-content/uploads/2020/12/empty_cart.png"
+      src="empty_cart.png"
       alt="empty cart"
     />
     <Text className="text-gray500">Adicione algo em seu carrinho...</Text>
