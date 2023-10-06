c427a93bdd1adcebd5139aa1b0c5f54376c130c8
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-06 18:07:51 -0300

    fix: add zindex in close button sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,10 +3,10 @@ import classNames from 'classnames'
 
 const styles = {
   sidebar:
-    'fixed top-0 right-0 h-screen w-96 overflow-y-auto z-[9999] px-8 pt-12 bg-gray900 block transition ease-in-out duration-[1000ms] h-full max-h-full overflow-hidden box-border',
+    'fixed top-0 right-0 h-screen w-96 overflow-y-auto z-[9000] px-8 pt-12 bg-gray900 block transition ease-in-out duration-[1000ms] h-full max-h-full overflow-hidden box-border',
   sidebarOpen: 'translate-x-0',
   sidebarClose: 'translate-x-full',
-  btnClose: 'absolute top-0 right-0 pt-10 pr-10',
+  btnClose: 'absolute top-0 right-0 pt-10 pr-10 z-[9999]',
 }
 
 type SidebarProps = {
