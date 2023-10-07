2f355e034d9e91936bf7727c384251f8e18f1b2e
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-07 15:42:12 -0300

    Add tests for Empty Cart

--- src/components/CartSidebar/components/EmptyCart/index.tsx
+++ src/components/CartSidebar/components/EmptyCart/index.tsx
@@ -1,9 +1,14 @@
 import { Text } from '@lebernardo/react'
+import styles from './styles'
 
 const EmptyCart = () => (
-  <div className="flex justify-center flex-col items-center h-5/6 overflow-hidden">
-    <img className="w-full px-8" src="empty_cart.png" alt="empty cart" />
-    <Text className="text-gray500">Adicione algo em seu carrinho...</Text>
+  <div className={styles.container}>
+    <img
+      className={styles.defaultImg}
+      src="/empty_cart.png"
+      alt="Imagem de carrinho vazio"
+    />
+    <Text className={styles.message}>Adicione algo em seu carrinho...</Text>
   </div>
 )
 
---
+++ src/components/CartSidebar/components/EmptyCart/styles.ts
@@ -0,0 +1,5 @@
+export default {
+  container: 'flex justify-center flex-col items-center h-5/6 overflow-hidden',
+  defaultImg: 'w-full px-8',
+  message: 'text-gray500',
+}
