62d5a5d54ddf516d4d5306922881c007cf439f28
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-09 19:24:55 -0300

    feat: resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
+const __dirname = path.resolve();
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@/root": path.join(__dirname, "."),
+      "@": path.join(__dirname, "src/"),
+    },
+  },
+});
