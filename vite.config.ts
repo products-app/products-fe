97a7f5d5a118e5bd31ffec6f35234d763397083f
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-09 20:19:50 -0300

    feat: add config to run tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-const __dirname = path.resolve();
+const __dirname = path.resolve()
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setup.ts'],
+  },
   resolve: {
     alias: {
-      "@/root": path.join(__dirname, "."),
-      "@": path.join(__dirname, "src/"),
+      '@/root': path.join(__dirname, '.'),
+      '@': path.join(__dirname, 'src/'),
     },
   },
-});
+})
