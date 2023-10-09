9d8c9fb53ee05862a5bb03237d7b894de44fb1e1
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-09 18:32:21 -0300

    feat: configure tailwind with default style

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
