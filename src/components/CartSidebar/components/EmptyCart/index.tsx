eafba91bdaab7d2f93c186dc17ef19f5274b0d57
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-09 21:54:49 -0300

    feat: add constants folder and set default images

--- src/components/CartSidebar/components/EmptyCart/index.tsx
+++ src/components/CartSidebar/components/EmptyCart/index.tsx
@@ -1,11 +1,12 @@
 import { Text } from '@lebernardo/react'
+import { defaultImages } from '@/constants/images'
 import styles from './styles'
 
 const EmptyCart = () => (
   <div className={styles.container}>
     <img
       className={styles.defaultImg}
-      src="/empty_cart.png"
+      src={defaultImages.emptyCart}
       alt="Imagem de carrinho vazio"
     />
     <Text className={styles.message}>Adicione algo em seu carrinho...</Text>
