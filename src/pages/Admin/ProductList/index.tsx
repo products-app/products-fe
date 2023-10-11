3e11dafee32fbab720c41c991c5f1e958f8bf0db
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-11 02:07:30 -0300

    feat: add crud for products

--- src/pages/Admin/ProductList/index.tsx
+++ src/pages/Admin/ProductList/index.tsx
@@ -1,6 +1,9 @@
 import Datatable from '@/components/Admin/DataTable'
 import { Heading, Card } from '@lebernardo/react'
-import { useStore } from '@/root/src/store/products'
+import { useNavigate } from 'react-router-dom'
+import { getProducts } from '@/api/products'
+import { useQuery } from 'react-query'
+import { useMemo } from 'react'
 
 const styles = {
   container: 'max-w-6xl m-auto py-12',
@@ -14,32 +17,30 @@ const styles = {
 const tableColumns = ['id', 'name', 'stock', 'price', 'active']
 
 function AdminProductList() {
-  const products = useStore((state) => state.products)
+  const navigate = useNavigate()
+  const { data } = useQuery(['products'], () => getProducts(''))
+  const products = useMemo(() => data?.data, [data])
+  const countProducts = products ? products.length : 0
 
   const handleRedirect = (item: object) => {
-    window.location.href = `/admin/add-product?id=${item.id}`
-  }
-
-  const handleRemove = (item: object) => {
-    console.log(item)
+    navigate(`/admin/add-product?id=${item.id}`)
   }
 
   return (
     <div className={styles.container}>
       <div className="text-center mb-4 font-heading">
         <Heading>
-          Produtos <span>23 itens cadastrados</span>
+          Produtos <span>{countProducts} itens cadastrados</span>
         </Heading>
       </div>
 
-      <Card className={styles.card}>
+      {products && (
         <Datatable
           columns={tableColumns}
           items={products}
           actionPrimary={handleRedirect}
-          actionSecondary={handleRemove}
         />
-      </Card>
+      )}
     </div>
   )
 }
