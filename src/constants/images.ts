bb363820242b5034b4ad86087dd24d559d183964
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-18 18:06:07 -0300

    fix: fix images path

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,4 @@
 export const defaultImages = {
-  productNotFound: 'product-not-found.jpg',
-  emptyCart: 'empty-cart.png',
+  productNotFound: '/product-not-found.jpg',
+  emptyCart: '/empty-cart.png',
 }
