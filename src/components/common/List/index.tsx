c28894e1c06dbef49464dc4159a026a45851e4bc
leticiabernardo <leticia.bernardo@mercadolivre.com>
2023-10-24 19:38:29 -0300

    refactor: list component

--- src/components/common/List/index.tsx
+++ src/components/common/List/index.tsx
@@ -1,12 +1,12 @@
 // TO DO: move to the design system
+import { LiContainer } from './styles'
+
 type ListProps = {
   children: React.ReactNode
 }
 
 const ListItem = ({ children }: ListProps) => (
-  <li className="flex gap-4 py-3 justify-between border-t-2 border-gray-700 border-solid">
-    {children}
-  </li>
+  <LiContainer>{children}</LiContainer>
 )
 
 const List = ({ children }: ListProps) => {
